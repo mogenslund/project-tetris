a51f5cbc1c10292d8366186c270a937560587c89
Mogens Brødsgaard Lund <salza@salza.dk>
2019-01-12 09:57:09 +0100

    Evaluation and configuration of editor

--- src/editor/core.clj
+++ src/editor/core.clj
@@ -1,7 +1,15 @@
 (ns editor.core
   (:require [clojure.string :as str]
+            [dk.salza.liq.apps.textapp :as textapp]
             [dk.salza.liq.core :as liq-core]
             [dk.salza.liq.editor :as editor]))
+
+(defn f5-eval
+  []
+  (editor/highlight-sexp-at-point)
+  (editor/eval-last-sexp)
+  (Thread/sleep 300)
+  (editor/highlight-sexp-at-point))
 
 (defn -main
   [& args]
@@ -9,9 +17,12 @@
   (liq-core/set-defaults)
   (apply liq-core/startup args)
   (liq-core/init-editor)
+  (editor/prompt-set "")
+  (editor/clear)
+  (editor/set-keymap @textapp/keymap-insert)
   (editor/add-rootfolder "./src")
   (editor/add-searchpath "./src")
-  (editor/insert (str/join "\n" (list
+  (editor/add-snippet (str/join "\n" (list
     "(vis)"
     "(firkant 0 0 \"sort\")"
     "(firkant 0 2 \"sort\")"
@@ -19,7 +30,11 @@
     "(doseq [r (range 1 15)] (sleep 500) (firkant (- r 1) 1 \"hvid\") (firkant r 1 \"sort\"))"
     "(skjul)"
     "(nulstil)")))
-  (editor/add-snippet "............................"))
+  (editor/add-snippet "(vis)")
+  (editor/add-snippet "(nulstil)")
+  (editor/add-snippet "(skjul)")
+  (editor/add-snippet "(firkant 0 0 \"blå\")")
+  (editor/set-global-key "f5" f5-eval))
 
 (ns user
   (:require [tetris.view :refer
